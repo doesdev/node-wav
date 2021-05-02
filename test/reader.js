dc2503964c55b04dc57f013072e19262443ff6be
Andrew Carpenter <andrew@audioinhd.com>
2021-05-01 21:28:16 -0400

    Extremely opinionated changes, i.e. not for upstream
    Specifically I eliminated any attempt at browser compatibility,
    switched to Standard style, and the only real functional addition was
    handling the JUNK bytes.

--- test/reader.js
+++ test/reader.js
@@ -4,140 +4,140 @@
  * Module dependencies.
  */
 
-var fs = require('fs');
-var path = require('path');
-var assert = require('assert');
-var Reader = require('../').Reader;
+const fs = require('fs')
+const path = require('path')
+const assert = require('assert')
+const Reader = require('../').Reader
 
 describe('Reader', function () {
   describe('RIFF - Little-endian', function () {
     describe('1up.wav', function () {
-      var fixture = path.resolve(__dirname, 'fixtures', '1up.wav');
+      const fixture = path.resolve(__dirname, 'fixtures', '1up.wav')
 
       it('should emit a "format" event', function (done) {
-        var reader = new Reader();
+        const reader = new Reader()
         reader.on('format', function (format) {
-          assert.equal(1, format.audioFormat);
-          assert.equal(1, format.channels);
-          assert.equal(11025, format.sampleRate);
-          assert.equal(8, format.bitDepth);
-          assert.equal(false, format.signed);
-          done();
-        });
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
+          assert.strictEqual(1, format.audioFormat)
+          assert.strictEqual(1, format.channels)
+          assert.strictEqual(11025, format.sampleRate)
+          assert.strictEqual(8, format.bitDepth)
+          assert.strictEqual(false, format.signed)
+          done()
+        })
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
 
       it('should emit an "end" event', function (done) {
-        var reader = new Reader();
-        reader.on('end', done);
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
-    });
+        const reader = new Reader()
+        reader.on('end', done)
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
+    })
 
     describe('gameover.wav', function () {
-      var fixture = path.resolve(__dirname, 'fixtures', 'gameover.wav');
+      const fixture = path.resolve(__dirname, 'fixtures', 'gameover.wav')
 
       it('should emit a "format" event', function (done) {
-        var reader = new Reader();
+        const reader = new Reader()
         reader.on('format', function (format) {
           // wave format
-          assert.equal('RIFF', this.riffId);
-          assert.equal(1, format.audioFormat);
+          assert.strictEqual('RIFF', this.riffId)
+          assert.strictEqual(1, format.audioFormat)
 
           // pcm format
-          assert.equal('LE', format.endianness);
-          assert.equal(1, format.channels);
-          assert.equal(22050, format.sampleRate);
-          assert.equal(16, format.bitDepth);
-          assert.equal(true, format.signed);
-          done();
-        });
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
+          assert.strictEqual('LE', format.endianness)
+          assert.strictEqual(1, format.channels)
+          assert.strictEqual(22050, format.sampleRate)
+          assert.strictEqual(16, format.bitDepth)
+          assert.strictEqual(true, format.signed)
+          done()
+        })
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
 
       it('should emit an "end" event', function (done) {
-        var reader = new Reader();
-        reader.on('end', done);
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
-    });
+        const reader = new Reader()
+        reader.on('end', done)
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
+    })
 
     describe('M1F1-float32-AFsp.wav', function () {
-      var fixture = path.resolve(__dirname, 'fixtures', 'M1F1-float32-AFsp.wav');
+      const fixture = path.resolve(__dirname, 'fixtures', 'M1F1-float32-AFsp.wav')
 
       it('should emit a "format" event', function (done) {
-        var reader = new Reader();
+        const reader = new Reader()
         reader.on('format', function (format) {
-          assert.equal(3, format.audioFormat);
-          assert.equal(2, format.channels);
-          assert.equal(8000, format.sampleRate);
-          assert.equal(32, format.bitDepth);
-          assert.equal(true, format.signed);
-          assert.equal(true, format.float);
-          done();
-        });
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
+          assert.strictEqual(3, format.audioFormat)
+          assert.strictEqual(2, format.channels)
+          assert.strictEqual(8000, format.sampleRate)
+          assert.strictEqual(32, format.bitDepth)
+          assert.strictEqual(true, format.signed)
+          assert.strictEqual(true, format.float)
+          done()
+        })
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
 
       it('should emit an "end" event', function (done) {
-        var reader = new Reader();
-        reader.on('end', done);
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
-    });
+        const reader = new Reader()
+        reader.on('end', done)
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
+    })
 
     describe('M1F1-float64-AFsp.wav', function () {
-      var fixture = path.resolve(__dirname, 'fixtures', 'M1F1-float64-AFsp.wav');
+      const fixture = path.resolve(__dirname, 'fixtures', 'M1F1-float64-AFsp.wav')
 
       it('should emit a "format" event', function (done) {
-        var reader = new Reader();
+        const reader = new Reader()
         reader.on('format', function (format) {
-          assert.equal(3, format.audioFormat);
-          assert.equal(2, format.channels);
-          assert.equal(8000, format.sampleRate);
-          assert.equal(64, format.bitDepth);
-          assert.equal(true, format.signed);
-          assert.equal(true, format.float);
-          done();
-        });
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
+          assert.strictEqual(3, format.audioFormat)
+          assert.strictEqual(2, format.channels)
+          assert.strictEqual(8000, format.sampleRate)
+          assert.strictEqual(64, format.bitDepth)
+          assert.strictEqual(true, format.signed)
+          assert.strictEqual(true, format.float)
+          done()
+        })
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
 
       it('should emit an "end" event', function (done) {
-        var reader = new Reader();
-        reader.on('end', done);
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
-    });
-  });
+        const reader = new Reader()
+        reader.on('end', done)
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
+    })
+  })
 
   describe('RIFX - Big-endian', function () {
     describe('gameover-rifx.wav', function () {
-      var fixture = path.resolve(__dirname, 'fixtures', 'gameover-rifx.wav');
+      const fixture = path.resolve(__dirname, 'fixtures', 'gameover-rifx.wav')
 
       it('should emit a "format" event', function (done) {
-        var reader = new Reader();
+        const reader = new Reader()
         reader.on('format', function (format) {
           // wave format
-          assert.equal('RIFX', this.riffId);
-          assert.equal(1, format.audioFormat);
+          assert.strictEqual('RIFX', this.riffId)
+          assert.strictEqual(1, format.audioFormat)
 
           // pcm format
-          assert.equal('BE', format.endianness);
-          assert.equal(1, format.channels);
-          assert.equal(22050, format.sampleRate);
-          assert.equal(16, format.bitDepth);
-          assert.equal(true, format.signed);
-          done();
-        });
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
+          assert.strictEqual('BE', format.endianness)
+          assert.strictEqual(1, format.channels)
+          assert.strictEqual(22050, format.sampleRate)
+          assert.strictEqual(16, format.bitDepth)
+          assert.strictEqual(true, format.signed)
+          done()
+        })
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
 
       it('should emit an "end" event', function (done) {
-        var reader = new Reader();
-        reader.on('end', done);
-        fs.createReadStream(fixture).pipe(reader).resume();
-      });
-    });
-  });
-});
+        const reader = new Reader()
+        reader.on('end', done)
+        fs.createReadStream(fixture).pipe(reader).resume()
+      })
+    })
+  })
+})
